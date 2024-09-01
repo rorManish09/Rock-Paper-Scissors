d0e2adfcb0779e7c40ea7aeea15ddd23281e75d3
rorManish09 <manishchaudhary4970@gmail.com>
2024-09-01 17:13:07 +0530

    Add new logic, More styling, New Images

--- script.js
+++ script.js
@@ -12,60 +12,88 @@ function getComputerChoice(){
   }
 }
 
-function getHumanChoice(){
+var humanScore = 0;
+var computerScore = 0;
 
-  userInput = prompt("Enter Your Choice in Numbers:\n Rock:1 Paper:2 Scissors:3")
-
-    const ChoiceObject ={
-      1:"Rock",
-      2:"Paper",
-      3:"Scissors"
-    }
-    
-    var valueChoice = Number(userInput)
-      
-  return ChoiceObject[valueChoice]
- }
-
- 
- var humanScore = 0;
- var computerScore = 0;
- 
  function playRound(humanSelection,computerSelection){
-
- 
+  
   if(humanSelection == computerSelection){
-
-    
-    return "Draw! Play Again. "
+   
+    return "Draw! Play Again."
   }
   else if (humanSelection == "Rock" & computerSelection == "Scissors" | humanSelection=='Paper' & computerSelection=="Rock" | humanSelection=="Scissors" & computerSelection=="Paper"){
     
     humanScore+=1
-    return "Human Score: "+ humanScore
+    return humanScore
+  }
+  else{    
+    computerScore+=1
+    return computerScore
+ }
+
+ }
+
+function winnerAnounce(){
+
+  
+  if(humanScore > computerScore){
+      return "Human Wins!"
+  }
+  else if(humanScore == computerScore  ){
+    return "Its a Draw."
   }
   else{
-    
-    computerScore+=1
-    return "Computer Score: " + computerScore
- }
- 
- }
-
-
- const playGame = () =>{
-
-  for(let i =0; i<5; i++){
-
-    const humanSelection = getHumanChoice()
-    const computerSelection = getComputerChoice()
-    var play= playRound(humanSelection,computerSelection)
-
-   console.log(play)
-    
+    return 'Computer Wins!'
   }
+}
 
- }
+  
+function drawLogic(input){
 
-playGame()
+  
+  if (input === "Draw! Play Again."){
+    return "Draw! Play Again."
+  }
+  else {
+    return ''
+  }    
+}
 
+
+const buttons = document.querySelectorAll('button');
+const hScore = document.getElementById('hScore');
+const cScore = document.getElementById('cScore');
+const roundNumber = document.getElementById('roundNum');
+const draw = document.getElementById('Draw')
+const winner = document.getElementById('winner-display')
+
+round = 1;
+buttons.forEach((buttons) => {
+buttons.addEventListener('click',() =>{
+    var newHumanSelection = buttons.id
+    if(newHumanSelection ){
+      
+      if(round>0 & round<=5){  
+        const computerSelection = getComputerChoice();
+        var play= playRound(newHumanSelection,computerSelection);
+        
+        draw.innerText = drawLogic(play)
+
+        hScore.innerText = humanScore;
+        cScore.innerText = computerScore;      
+        round++;
+        
+        
+      }
+      
+    }
+    
+    if(round<=5){
+      roundNumber.innerText = round;
+    }
+    else{
+      roundNumber.innerText = 5;
+      winner.innerText = winnerAnounce()
+    }
+  })
+})
